98dab6a0720e9c052af75ce4ed1208968a12c4b5
Bo Lundehave Nyby <201906213@post.au.dk>
2022-10-07 14:41:17 +0200

    Now able to draw points and lines

--- projective_plane_duality/src/App.js
+++ projective_plane_duality/src/App.js
@@ -6,15 +6,38 @@ export default class App extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      primalObjects: ["p(150,8)", "p(1, 55)", "p(250, 370)"]
+      // primalObjects: ["p(150,8)", "p(1, 55)", "p(250, 370)"]
+      primalObjects: [["p", 20, 200], ["p", 5, 55]]
     };
+  }
+
+  dualizeObjects() {
+    const objects = [];
+
+    this.state.primalObjects.forEach(o => {
+      const type = o[0];
+      switch (type) {
+        case "p":
+          // const regex = /\(|\)|\s/ig; // Regex (global) to remove all '(', ')' and ' '
+          // const [x, y] = val.replaceAll(regex, "").split(",");
+          const x = o[1];
+          const y = o[2];
+          objects.push(["l", x, -y]) // p(p_x,p_y) => y=p_x*x-p_y
+          break;
+        default:
+          console.log("SADNESS!");
+          break;
+      }
+    });
+
+    return objects;
   }
 
   render() {
     return (
       <div className="App">
         <View title="Primal" objects={this.state.primalObjects} />
-        <View title="Dual" objects={this.state.primalObjects} />
+        <View title="Dual" objects={this.dualizeObjects(this.state.primalObjects)} />
       </div>
     )
   }
--- projective_plane_duality/src/views.js
+++ projective_plane_duality/src/views.js
@@ -27,11 +27,33 @@ export default class View extends PureComponent {
 
 
         this.props.objects.forEach(o => {
-            const [type, val] = this.split(o, 1);
-            const regex = /\(|\)|\s/ig; // Regex (global) to remove all '(', ')' and ' '
-            const [x, y] = val.replaceAll(regex, "").split(",");
+            // const [type, val] = this.split(o, 1);
+            // const regex = /\(|\)|\s/ig; // Regex (global) to remove all '(', ')' and ' '
+            // const [x, y] = val.replaceAll(regex, "").split(",");
 
-            ctx.fillRect(x, y, 5, 5);
+            const type = o[0];
+
+            switch (type) {
+                case "p":
+                    const x = o[1];
+                    const y = o[2];
+                    ctx.fillRect(x, y, 5, 5);
+                    break;
+                case "l":
+                    const p_x = o[1];
+                    const p_y = o[2];
+                    const start_y = -1000 * p_x + p_y;
+                    const end_y = 1000 * p_x + p_y;
+                    ctx.beginPath();
+                    ctx.strokeWidth = 2;
+                    ctx.moveTo(-1000, start_y);
+                    ctx.lineTo(1000, end_y);
+                    ctx.stroke();
+                    break;
+                default:
+                    console.log("SADNESS INSIDE OF VIEW: " + type);
+            }
+
         });
     }
 
